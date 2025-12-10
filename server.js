4a57f66ac319a87c1b18ce96ed6c700caa7e8beb
Kim Dudenhöfer <kidu2400@student.miun.se>
2025-12-10 14:49:28 +0100

    Aktiverat cors för samtliga routes.

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ require('dotenv').config();
 const init = async () => {
     const server = Hapi.server({
         port: 5000,
-        host: '0.0.0.0'
+        host: '0.0.0.0',
+        routes: {
+            cors: {
+                origin: ['*']
+            }
+        }
     });
 
     //Getting db-connection
